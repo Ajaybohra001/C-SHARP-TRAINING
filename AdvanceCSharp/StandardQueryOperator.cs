576ad1cf47d93075e60199308608734476a9a7f8
SUCCESSIVE\ajay.bohra <ajay.bohra@successive.tech>
2023-11-30 17:47:09 +0530

    updated the groupby,tolookup,deferred and immediate

--- AdvanceCSharp/StandardQueryOperator.cs
+++ AdvanceCSharp/StandardQueryOperator.cs
@@ -13,6 +13,13 @@ namespace StandardQueryOperators
         public int StudentID { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
+        public int courseId { get; set; }
+
+    }
+    public class Course
+    {
+        public int courseId { get; set; }
+        public string courseName { get; set; }
 
     }
     class Program
@@ -221,24 +228,210 @@ namespace StandardQueryOperators
             //Concat
 
             //List<int> integer1List = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
-            //List<int> integer2List = new List<int>() { 8,9,10 };
+            //List<int> integer2List = new List<int>() { 7,8, 9, 10 };
 
             //var concatList = integer1List.Concat(integer2List);
 
-            //foreach(var i in concatList)
+            //foreach (var i in concatList)
             //{
             //    Console.WriteLine(i);
             //}
 
             //Distinct
 
-            List<int> duplicateList = new List<int>() { 1, 2, 3, 2, 4, 3, 5, 6, 6 };
-            var distinctList = duplicateList.Distinct();
+            //List<int> duplicateList = new List<int>() { 1, 2, 3, 2, 4, 3, 5, 6, 6 };
+            //var distinctList = duplicateList.Distinct();
 
-            foreach (var i in distinctList)
+            //foreach (var i in distinctList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Except
+
+            //List<string> list1 = new List<string>() {"one","two","three","four"};
+            //List<string> list2 = new List<string>() {"two","three"};
+
+            //var exceptList = list1.Except(list2).Aggregate((s1, s2) => s1 + "," + s2);
+
+            //foreach (var i in exceptList)
+            //    Console.Write( i);
+
+
+            //List<Student> studentList1 = new List<Student>() {   new Student() { StudentID = 1, StudentName = "Ajay", Age = 18 } ,
+            //                                                     new Student() { StudentID = 2, StudentName = "Akshsy",  Age = 15 } ,
+            //                                                     new Student() { StudentID = 5, StudentName = "Prince" , Age = 25 },
+
+            //                                                     new Student() { StudentID = 3, StudentName = "Govind",  Age = 25 } ,
+            //                                                     new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20 } ,
+            //                                                     new Student() { StudentID = 5, StudentName = "Prince" , Age = 19 }
+            //                                                  };
+
+
+            //List<Student> studentList2 = new List<Student>()
+            //{
+            //     new Student() { StudentID = 5, StudentName = "Prince" , Age = 19 },
+            //      new Student() { StudentID = 3, StudentName = "Govind",  Age = 25 } ,
+            //      new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20 } 
+            //};
+
+            //var exceptResult = studentList1.Except(studentList2, new StudentComparer());
+
+            //foreach (var i in exceptResult)
+            //    Console.WriteLine(i.StudentName);
+
+            //Intersect
+            //List<int> intersectList1 = new List<int>{ 1, 2, 3, 4, 5 };
+            //List<int> intersectList2 = new List<int> { 2, 3, 4 };
+
+            //var intersecList = intersectList1.Intersect(intersectList2);
+
+            //foreach(var i in intersecList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Union
+
+            //List<int> unionList1 = new List<int> { 1, 2, 3, 4, 5 };
+
+            //List<int> unionList2 = new List<int> { 2, 3, 4 };
+
+            //var unionList = unionList1.Union(unionList2);
+
+            //foreach (var i in unionList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Join
+
+            //List<Student> stud = new List<Student>()
+            //{
+            //    new Student() { StudentID = 1, StudentName = "Ajay", Age = 18,courseId=101 } ,
+            //    new Student() { StudentID = 2, StudentName = "Akshsy",  Age = 15,courseId=102 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 25,courseId=103 },
+
+            //    new Student() { StudentID = 3, StudentName = "Govind",  Age = 25 ,courseId=104} ,
+            //    new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20,courseId=105 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 19,courseId=106 }
+            //};
+
+            //List<Course> course = new List<Course>()
+            //{
+            //    new Course(){courseId=101,courseName="MCA"},
+            //    new Course(){courseId=102,courseName="Btech"}
+            //};
+
+            //var joinResult = stud.Join(course,
+            //                            student=>student.courseId,c=>c.courseId,
+            //                            (student, c) => new
+            //                            {
+            //                                studentName=student.StudentName,
+            //                                course=c.courseName
+            //                            }
+
+
+            //    );
+
+            //foreach(var i in joinResult)
+            //{
+            //    Console.WriteLine("Student Name : {0} Course Name : {1}", i.studentName, i.course);
+            //}
+
+            //Deferred
+
+            //List<Student> stud = new List<Student>()
+            //{
+            //    new Student() { StudentID = 1, StudentName = "Ajay", Age = 18,courseId=101 } ,
+            //    new Student() { StudentID = 2, StudentName = "Akshsy",  Age = 15,courseId=102 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 25,courseId=103 },
+
+            //    new Student() { StudentID = 3, StudentName = "Govind",  Age = 25 ,courseId=104} ,
+            //    new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20,courseId=105 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 19,courseId=106 }
+            //};
+
+            //var checkAge = stud.Where(s => s.Age < 20);
+
+            //stud.Add(new Student() { StudentID = 6, StudentName = "Ram", Age = 10, courseId = 107 });
+
+            //foreach(var i in checkAge)
+            //Console.WriteLine(i.StudentName);
+
+            //Immediate
+
+            //List<Student> stud = new List<Student>()
+            //{
+            //    new Student() { StudentID = 1, StudentName = "Ajay", Age = 18,courseId=101 } ,
+            //    new Student() { StudentID = 2, StudentName = "Akshsy",  Age = 15,courseId=102 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 25,courseId=103 },
+
+            //    new Student() { StudentID = 3, StudentName = "Govind",  Age = 25 ,courseId=104} ,
+            //    new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20,courseId=105 } ,
+            //    new Student() { StudentID = 5, StudentName = "Prince" , Age = 19,courseId=106 }
+            //};
+
+            //var checkAge = stud.Where(s => s.Age < 20).ToList();
+
+            //stud.Add(new Student() { StudentID = 6, StudentName = "Ram", Age = 10, courseId = 107 });
+
+            //foreach (var i in checkAge)
+            //    Console.WriteLine(i.StudentName);
+
+            //Groupby
+
+            List<Student> stud = new List<Student>()
             {
-                Console.WriteLine(i);
-            }
+                new Student() { StudentID = 1, StudentName = "Ajay", Age = 18,courseId=101 } ,
+                new Student() { StudentID = 2, StudentName = "Akshsy",  Age = 15,courseId=102 } ,
+                new Student() { StudentID = 5, StudentName = "Prince" , Age = 18,courseId=103 },
+
+                new Student() { StudentID = 3, StudentName = "Govind",  Age = 15 ,courseId=104} ,
+                new Student() { StudentID = 4, StudentName = "Utkarsh" , Age = 20,courseId=105 } ,
+                new Student() { StudentID = 5, StudentName = "Prince" , Age = 20,courseId=106 }
+            };
+
+            //var groupedResult = stud.GroupBy(s=>s.Age);
+       
+            //foreach (var ageGroup in groupedResult)
+            //{
+            //    Console.WriteLine("Age Group: {0}", ageGroup.Key); 
+
+            //    foreach (Student s in ageGroup) 
+            //        Console.WriteLine("Student Name: {0}", s.StudentName);
+            //}
+
+            //Lookup
+
+            //var lookUpResult = stud.ToLookup(s => s.Age);
+
+            //foreach (var ageGroup in lookUpResult)
+            //{
+            //    Console.WriteLine("Age Group: {0}", ageGroup.Key);
+
+            //    foreach (Student s in ageGroup)
+            //        Console.WriteLine("Student Name: {0}", s.StudentName);
+            //}
+
+            //ElementAt and ElementAtOrDefault
+
+            List<int> intList = new List<int>() { 10, 21, 30, 45, 50, 87 };
+            List<string> strList = new List<string>() { "One", "Two", null, "Four", "Five" };
+
+            Console.WriteLine("1st Element in intList: {0}", intList.ElementAt(0));
+            Console.WriteLine("1st Element in strList: {0}", strList.ElementAt(0));
+
+            Console.WriteLine("2nd Element in intList: {0}", intList.ElementAt(1));
+            Console.WriteLine("2nd Element in strList: {0}", strList.ElementAt(1));
+
+            Console.WriteLine("3rd Element in intList: {0}", intList.ElementAtOrDefault(10));
+            Console.WriteLine("3rd Element in strList: {0}", strList.ElementAtOrDefault(2));
+
+
+
+
+
 
 
 
@@ -254,6 +447,21 @@ namespace StandardQueryOperators
 
         
 
+        }
+        class StudentComparer : IEqualityComparer<Student>
+        {
+            public bool Equals(Student x, Student y)
+            {
+                if (x.StudentID == y.StudentID && x.StudentName.ToLower() == y.StudentName.ToLower())
+                    return true;
+
+                return false;
+            }
+
+            public int GetHashCode(Student obj)
+            {
+                return obj.StudentID.GetHashCode();
+            }
         }
     }
 }
