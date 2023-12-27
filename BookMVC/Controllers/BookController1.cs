d52c308ce3e047fca5f2e773848ebfc7384c107f
SUCCESSIVE\ajay.bohra <ajay.bohra@successive.tech>
2023-12-27 22:13:53 +0530

    perform mvc crud operations

---
+++ BookMVC/Controllers/BookController1.cs
@@ -0,0 +1,85 @@
+﻿using BookMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMVC.Controllers
+{
+    public class BookController1 : Controller
+    {
+        private List<Book> _books = new List<Book>
+    {
+        new Book { bookId = 1, bookName = "Book 1", bookAuthor = "Author 1" },
+        new Book { bookId = 2, bookName = "Book 2", bookAuthor= "Author 2" }
+       
+    };
+        public IActionResult Index()
+        {
+            return View(_books);
+        }
+        public IActionResult Details(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.bookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            
+            book.bookId = _books.Max(b => b.bookId) + 1;
+            _books.Add(book);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.bookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            var existingBook = _books.FirstOrDefault(b => b.bookId== book.bookId);
+            if (existingBook != null)
+            {
+                existingBook.bookName = book.bookName;
+                existingBook.bookAuthor = book.bookAuthor;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.bookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.bookId== id);
+            if (book != null)
+            {
+                _books.Remove(book);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookMVC/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace BookMVC.Models
+{
+    public class Book
+    {
+        public int bookId {  get; set; }
+        public string bookName { get; set; }    
+        public string bookAuthor {  get; set; }
+    }
+}
